3c56bcb363f9f473360cc007cb9e380768269668
icecream <158474361@qq.com>
2019-12-28 00:14:12 -0800

    packaged_task

---
+++ future/packaged_task.cpp
@@ -0,0 +1,20 @@
+#include <math.h>
+#include <stdio.h>
+
+#include <future>
+#include <iostream>
+
+void task_lambda(){
+    std::packaged_task<int(int,int)> task([](int a, int b) {
+        return std::pow(a, b); 
+    });
+    std::future<int> result = task.get_future();
+ 
+    task(2, 9);
+ 
+    std::cout << "task_lambda:\t" << result.get() << '\n';
+}
+
+int main(void){
+    task_lambda();
+}
