4d8d6b3da0f3866dc927bad806570e5e06ad917f
icecream <158474361@qq.com>
2020-03-21 22:51:12 -0700

    t_namespace

--- Tcpp/T_namespace.cpp
+++ Tcpp/T_namespace.cpp
@@ -25,12 +25,14 @@ void f(){
 int j = 0;
 namespace man_ns{
     void manip(){
-        using namespace blip; //把blip中的symbols注入 全局作用域
+        using namespace blip; //把blip中的symbols注入 全局作用域(因为全局作用域是最近的同时包含using指令和命名空间blip的作用域)
+        //every name from ns_name is visible as if 
+        //it were declared in the nearest enclosing namespace which contains both the using-directive and ns_name.
         ++i;
         // ++j; 有二义性
         ++::j;
         ++blip::j;
-        ++l; //Declarations inside inline ns_name will be visible in its enclosing namespace. (enclosing 仅仅是外面一层作用域)
+        ++l; //Declarations inside inline ns_name will be visible in its nearest enclosing namespace. (nearest enclosing 仅仅是外面一层作用域)
         int k = 97;
         ++k;
     }
@@ -46,10 +48,30 @@ namespace foo {
     }
     void foo_qux(){
         //error: ‘qux’ was not declared in this scope
-        int a = qux; //qux is undefined;
+        //int a = qux; //qux is undefined;
     }
 }
 
+namespace a{
+    int j = 10;
+
+    namespace b1{
+        int j = 16;
+    }
+
+    namespace b{
+        void manij(){
+            using namespace b1; //(如果是注入全局作用域 那么这个j就不会有二义性 j应该为a::j)
+            //every name from ns_name is visible as if 
+            //it were declared in the nearest enclosing namespace which contains both the using-directive and ns_name.
+
+            //++j;
+        }
+    }
+    
+    
+    
+}
 int main(void){
 
 }
